84488debf2c8be3911341563f0f41356f560d750
xyt <xiayutai@hongte.info>
2017-12-15 18:07:07 +0800

    增加配置中心项目

---
+++ config-client-app/src/main/java/com/ht/config/client/app/ConfigClientApplication.java
@@ -0,0 +1,21 @@
+package com.ht.config.client.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * 摘要:
+ *
+ * @author xyt
+ * @create 2017-12-14 下午8:30
+ **/
+
+@SpringCloudApplication
+public class ConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+}
+
