5e28b9782c42a9fe75106a8737bc6292820f3c95
xyt <xiayutai@hongte.info>
2017-12-15 17:51:06 +0800

    增加配置中心项目

---
+++ ussp-config-app/src/main/java/com/ht/ussp/config/app/ConfigApplication.java
@@ -0,0 +1,20 @@
+package com.ht.ussp.config.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * 摘要:配置中心
+ *
+ * @author xyt
+ * @create 2017-12-14 下午2:59
+ **/
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ConfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+}
