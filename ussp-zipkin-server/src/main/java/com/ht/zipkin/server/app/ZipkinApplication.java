f8e8d8b0cb7baf73f5a50ca1ff131bffc3de67e7
xyt <xiayutai@hongte.info>
2017-12-19 14:25:26 +0800

    引入路径追踪应用sleuth

---
+++ ussp-zipkin-server/src/main/java/com/ht/zipkin/server/app/ZipkinApplication.java
@@ -0,0 +1,24 @@
+package com.ht.zipkin.server.app;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
+
+/**
+ * 摘要:zipkin路径跟踪
+ *
+ * @author xyt
+ * @create 2017-12-18 下午4:04
+ **/
+
+@EnableZipkinStreamServer
+@SpringBootApplication
+public class ZipkinApplication {
+    private static final Logger LOG = LoggerFactory.getLogger(ZipkinApplication.class);
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkinApplication.class, args);
+        LOG.info("ZipkinApplication-> ZipkinApplication start  Success !!!");
+    }
+}
