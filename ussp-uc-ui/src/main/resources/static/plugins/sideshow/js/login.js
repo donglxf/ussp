e936d870abeb8ca39266da05142dac4c73404bc1
qiuwenwu@hongte.info <qiuwenwu@hongte.info>
2018-01-19 19:09:58 +0800

    1、登录 2、退出 3、COOKIE操作

--- ussp-uc-ui/src/main/resources/static/plugins/sideshow/js/login.js
+++ ussp-uc-ui/src/main/resources/static/plugins/sideshow/js/login.js
@@ -23,8 +23,10 @@ function login() {
 				success : function(data) {
 					var token = data.token;
 					var refreshToken = data.refreshToken;
-					document.cookie = "token=" + token;
-					document.cookie = "refreshToken=" + refreshToken;
+					setCookie("token",token,12,"/");
+					setCookie("refreshToken",refreshToken,700,"/");
+//					document.cookie = "token=" + token;
+//					document.cookie = "refreshToken=" + refreshToken;
 					location.href = '/';
 				},
 				error : function(xhr, exception, errorThrown) {
@@ -32,8 +34,12 @@ function login() {
 							|| xhr.status == '0') {
 						
 						alert("用户名或密码错误，请重新登录");
-						$(':input','#loginForm').not(':button,:submit,:reset,:hidden').val('').removeAttr('checked').removeAttr('checked')
+						
 					}
+					if(xhr.status=='500'){
+						alert("您无权登录，请联系管理员");
+					}
+					$(':input','#loginForm').not(':button,:submit,:reset,:hidden').val('').removeAttr('checked').removeAttr('checked');
 				}
 
 			});
@@ -41,3 +47,58 @@ function login() {
 	return true;
 
 }
+
+
+function logout(){
+	deleteCookie("token","/");
+	deleteCookie("refreshToken","/");
+	window.location.href="/login.html?backurl="+window.location.href; 
+	
+}
+
+//新建cookie。 
+//hours为空字符串时,cookie的生存期至浏览器会话结束。hours为数字0时,建立的是一
+//个失效的cookie,这个cookie会覆盖已经建立过的同名、同path的cookie（如果这个
+//cookie存在）。 
+function setCookie(name,value,times,path){
+ var name = escape(name);
+ var value = escape(value);
+ var expires = new Date(); 
+ expires.setMinutes(expires.getMinutes() + times);
+ path = path == "" ? "" : ";path=" + path; 
+ _expires = (typeof times) == "string" ? "" : ";expires=" + expires.toUTCString();
+ document.cookie = name + "=" + value + _expires + path;
+} 
+//获取cookie值 
+function getCookieValue(name){
+ var name = escape(name); 
+ //读cookie属性，这将返回文档的所有cookie
+ var allcookies = document.cookie;      
+ //查找名为name的cookie的开始位置
+ name += "="; 
+ var pos = allcookies.indexOf(name);    
+ //如果找到了具有该名字的cookie，那么提取并使用它的值 
+ //如果pos值为-1则说明搜索"version="失败
+ if (pos != -1){    
+		   //cookie值开始的位置    
+		   var start = pos + name.length;  
+		   //从cookie值开始的位置起搜索第一个";"的位置,即cookie值结尾的位置 	   
+		   var end = allcookies.indexOf(";",start); 
+		   //如果end值为-1说明cookie列表里只有一个cookie        
+		   if (end == -1) end = allcookies.length;   
+		   //提取cookie的值	   
+		   var value = allcookies.substring(start,end);  
+		   //对它解码   
+		   return unescape(value);                            
+     }   
+ else return "";                                             
+} 
+//删除cookie 
+function deleteCookie(name,path){
+ var name = escape(name);
+ var expires = new Date(0); 
+ path = path == "" ? "" : ";path=" + path; 
+ document.cookie = name + "="+ ";expires=" + expires.toUTCString() + path;
+} 
+  
+    
