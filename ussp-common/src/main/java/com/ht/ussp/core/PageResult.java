f98a2848e778749377c21566fe2d40bb8447779a
谭荣巧 <tanrongqiao@hongte.info>
2018-01-25 21:37:21 +0800

    增加方法

--- ussp-common/src/main/java/com/ht/ussp/core/PageResult.java
+++ ussp-common/src/main/java/com/ht/ussp/core/PageResult.java
@@ -35,8 +35,23 @@ public class PageResult<D> extends Result<D> {
      * @author 谭荣巧
      * @Date 2018/1/11 15:58
      */
-    public PageResult count(long count) {
+    public PageResult<D> count(long count) {
         this.count = count;
         return this;
+    }
+
+    /**
+     * 构建成功结果对象<br>
+     *
+     * @param data  返回的数据
+     * @param count 总记录数
+     * @return 分页结果对象
+     * @author 谭荣巧
+     * @Date 2018/1/25 21:36
+     */
+    public static <D> PageResult<D> buildSuccess(D data, long count) {
+        PageResult<D> result = new PageResult<>();
+        result.count(count).returnCode(ReturnCodeEnum.SUCCESS.getReturnCode()).codeDesc(ReturnCodeEnum.SUCCESS.getCodeDesc()).data(data);
+        return result;
     }
 }
