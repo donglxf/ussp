f31c0a4e2b176e831b04b1540d77a97f0f73f4e3
谭荣巧 <tanrongqiao@hongte.info>
2018-02-01 15:54:58 +0800

    网关接入配置中心

--- ussp-uc-ui/src/main/resources/static/plugins/layui/extend/modules/ht_config.js
+++ ussp-uc-ui/src/main/resources/static/plugins/layui/extend/modules/ht_config.js
@@ -2,7 +2,7 @@
  * add by tanrq 2018/1/21
  */
 layui.define(function (exports) {
-    var basePath = "http://localhost:9000/",
+    var basePath = "http://10.110.1.240:30111/",
         rule = "uc";
     exports('ht_config', {
         app: "UC"
