70702f8a6dfdbef5270d68e1d796d1dfb642eb79
leiricong <leiricong@hongte.info>
2018-02-10 18:36:48 +0800

    修复bug

--- ussp-uc-app/src/main/java/com/ht/ussp/uc/app/service/HtBoaInOrgService.java
+++ ussp-uc-app/src/main/java/com/ht/ussp/uc/app/service/HtBoaInOrgService.java
@@ -58,7 +58,7 @@ public class HtBoaInOrgService {
         String search = "";
         String orgPath = "";
         if (query != null && query.size() > 0 && query.get("orgCode") != null) {
-        	orgPath = "%" +query.get("orgCode")+ "%";
+        	orgPath = query.get("orgCode");
         }
         if (query != null && query.size() > 0 && query.get("keyWord") != null) {
         	search = "%" +query.get("keyWord")+ "%";
