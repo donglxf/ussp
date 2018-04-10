025d20380c2bb88197b2ab48cc2904255aa22a35
qiuwenwu@hongte.info <qiuwenwu@hongte.info>
2018-04-10 10:35:57 +0800

    为uc添加自定义token&refreshToken接口

--- ussp-uaa-app/src/main/java/com/ht/ussp/uaa/app/resource/ExternalTokenManager.java
+++ ussp-uaa-app/src/main/java/com/ht/ussp/uaa/app/resource/ExternalTokenManager.java
@@ -1,8 +1,11 @@
 package com.ht.ussp.uaa.app.resource;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 import javax.servlet.http.HttpServletResponse;
@@ -10,12 +13,15 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ht.ussp.common.SysStatus;
 import com.ht.ussp.uaa.app.config.JwtSettings;
 import com.ht.ussp.uaa.app.exception.JwtExpiredTokenException;
@@ -47,7 +53,9 @@ public class ExternalTokenManager {
 	private TokenExtractor tokenExtractor;
 	@Autowired
 	private JwtSettings jwtSettings;
-
+	@Autowired
+	private ObjectMapper mapper;
+	
 	@PostMapping(value = "/createAppToken")
 	@ApiOperation(value = "创建系统级别token")
 	public AccessJwtToken createToken(String app) {
@@ -96,4 +104,66 @@ public class ExternalTokenManager {
 		}
 		return rm;
 	}
+	
+	
+	/**
+	 * 
+	 * @Title: createUCToken 
+	 * @Description: 给UC使用，与信贷系统有关，创建token和refreshToken 
+	 * @return AccessJwtToken
+	 * @throws
+	 * @author wim qiuwenwu@hongte.info 
+	 * @date 2018年4月10日 上午9:47:18
+	 */
+	@PostMapping(value = "/createUCToken")
+	@ApiOperation(value = "创建UCToken")
+	public void createUCToken(String userId,String bmUserId,Integer tokenTime,Integer refreshtime,HttpServletResponse response) {
+
+		if (StringUtils.isBlank(userId)) {
+			throw new IllegalArgumentException("Cannot create JWT Token without userId");
+
+		}else if(StringUtils.isBlank(bmUserId)) {
+			throw new IllegalArgumentException("Cannot create JWT Token without bmUserId");
+
+		}else if (refreshtime.intValue()<=0) {
+			throw new IllegalArgumentException("refreshtime must greater than 0");
+
+		}
+		
+		Map<String, String> tokenMap = new HashMap<String, String>();
+		LocalDateTime currentTime = LocalDateTime.now();
+
+		Claims claims = Jwts.claims().setSubject("UC jwt token");
+		claims.put("userId", userId);
+		claims.put("bmUserId", bmUserId);
+		claims.put("refreshtime", refreshtime);
+		String token = Jwts.builder().setClaims(claims).setIssuer(jwtSettings.getTokenIssuer())
+				.setIssuedAt(Date.from(currentTime.atZone(ZoneId.systemDefault()).toInstant()))
+				.setExpiration(Date.from(currentTime.plusMinutes(tokenTime)
+						.atZone(ZoneId.systemDefault()).toInstant()))
+				.signWith(SignatureAlgorithm.HS512, jwtSettings.getTokenSigningKey())
+				.compact();
+		
+		String refreshToken = Jwts.builder().setClaims(claims).setIssuer(jwtSettings.getTokenIssuer())
+				.setId(UUID.randomUUID().toString())
+				.setIssuedAt(Date.from(currentTime.atZone(ZoneId.systemDefault()).toInstant()))
+				.setExpiration(Date.from(currentTime.plusMinutes(refreshtime)
+						.atZone(ZoneId.systemDefault()).toInstant()))
+				.signWith(SignatureAlgorithm.HS512, jwtSettings.getTokenSigningKey()).compact();
+		log.info("uc's token and refreshToken has created sucessful");
+		tokenMap.put("token", token);
+		tokenMap.put("refreshToken", refreshToken);
+		
+		response.setStatus(HttpStatus.OK.value());
+		response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+		try {
+			mapper.writeValue(response.getWriter(), tokenMap);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	
+	
 }
