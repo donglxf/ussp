6a6d735d55f47e816eb5f820a223f1c7bc96cd6b
qiuwenwu@hongte.info <qiuwenwu@hongte.info>
2018-04-27 15:06:54 +0800

    开启uaa hystrix,更新UC依赖的common版本

--- ussp-uaa-app/src/main/java/com/ht/ussp/uaa/app/feignClient/UserClient.java
+++ ussp-uaa-app/src/main/java/com/ht/ussp/uaa/app/feignClient/UserClient.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.ht.ussp.uaa.app.model.ResponseModal;
-import com.ht.ussp.uaa.app.vo.UserVo;
 
 /**
  * 
