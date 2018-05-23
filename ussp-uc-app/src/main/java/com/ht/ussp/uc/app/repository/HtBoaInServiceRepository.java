d801871acdd8a095c2427739cc1fdb0372c66f48
leiricong <leiricong@hongte.info>
2018-05-23 11:05:36 +0800

    提交

--- ussp-uc-app/src/main/java/com/ht/ussp/uc/app/repository/HtBoaInServiceRepository.java
+++ ussp-uc-app/src/main/java/com/ht/ussp/uc/app/repository/HtBoaInServiceRepository.java
@@ -13,7 +13,7 @@ public interface HtBoaInServiceRepository  extends JpaSpecificationExecutor<HtBo
 	 @Query(value = "SELECT * FROM ( " +
 	            "SELECT " +
 	            "service_code,application_service_name,application_service, app parentapp,app " +
-	            "FROM ht_boa_in_service " +
+	            "FROM HT_BOA_IN_SERVICE " +
 	            "WHERE STATUS='0' " +
 	            "UNION ALL " +
 	            "SELECT " +
